0de336a73a3c2cdc60e62c53895c5b90b4f9769c
danielaciuperca <daniela.ciuperca20@gmail.com>
2021-04-13 15:57:14 +0300

    Lab 9

--- lab9-ex1/src/streams/model/Client.java
+++ lab9-ex1/src/streams/model/Client.java
@@ -10,7 +10,31 @@ public class Client {
         this.address = address;
         this.email = email;
     }
-    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String toString() {
         return name + " / " + address + " / " + email;
